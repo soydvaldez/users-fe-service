d237cbefde3b3ffefa81aa738c33440c81ecb496
soydvaldez <soydvaldez@gmail.com>
2024-12-23 12:20:16 -0600

    Feat: #000002: Implement interactive sidebar with expandable/collapsible sections - Added sidebar with sections that can be collapsed or expanded via a button. - When collapsed, only icons are shown, and when expanded, section labels are revealed. - Each section contains a set of submenus. - Implemented reactive state management to preserve section states; if a section was open before collapse, it will reopen automatically when expanded again. - Improved user experience and optimized screen real estate.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -11,7 +11,44 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class SidebarComponent {
   isSidebarVisible: boolean = false;
+  isUserOptionsActive: boolean = false;
+  isRoleOptionsActive: boolean = false;
+  isDashboardOptionsActive: boolean = false;
+
+  // state sections
+  sectionsState = {
+    usuarios: false,
+    roles: false,
+    dashbboard: false,
+    reportes: false,
+    seguridad: false,
+  };
+
   toggleSidebar() {
+    if (!this.isSidebarVisible) {
+      this.isUserOptionsActive = this.sectionsState['usuarios'];
+      this.isRoleOptionsActive = this.sectionsState['roles'];
+      this.isDashboardOptionsActive = this.sectionsState['dashbboard'];
+    } else {
+      this.isUserOptionsActive = false;
+      this.isRoleOptionsActive = false;
+      this.isDashboardOptionsActive = false;
+    }
     this.isSidebarVisible = !this.isSidebarVisible;
+  }
+
+  toggleUsersOptions() {
+    this.isUserOptionsActive = !this.isUserOptionsActive;
+    this.sectionsState['usuarios'] = this.isUserOptionsActive;
+  }
+
+  toggleRolesOptions() {
+    this.isRoleOptionsActive = !this.isRoleOptionsActive;
+    this.sectionsState['roles'] = this.isRoleOptionsActive;
+  }
+
+  toggleDashboardOptions() {
+    this.isDashboardOptionsActive = !this.isDashboardOptionsActive;
+    this.sectionsState['dashbboard'] = this.isDashboardOptionsActive;
   }
 }
